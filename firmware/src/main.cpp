c1e08905ee9fa5e78a2777f94bd45ed03c0e864e
Medion2 <165664097+Medion2@users.noreply.github.com>
2026-07-04 15:24:30 +0200

    Add initial main firmware entrypoint

---
+++ firmware/src/main.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include "version.h"
+#include "logger.h"
+#include "wifi_manager.h"
+
+static unsigned long lastHeartbeat = 0;
+
+static void printBootHeader() {
+  Serial.println();
+  Serial.println("========================================");
+  Serial.println(VEG_NAME);
+  Serial.print("Version   : ");
+  Serial.println(VEG_VERSION);
+  Serial.print("Codename  : ");
+  Serial.println(VEG_CODENAME);
+  Serial.println("Hardware  : ESP32-S3 DevKitC-1 N16R8");
+  Serial.println("========================================");
+  Serial.println();
+}
+
+static void printSystemInfo() {
+  logInfo(String("Chip Model: ") + ESP.getChipModel());
+  logInfo(String("CPU MHz: ") + String(ESP.getCpuFreqMHz()));
+  logInfo(String("Flash Size: ") + String(ESP.getFlashChipSize() / 1024 / 1024) + " MB");
+  logInfo(String("Heap Free: ") + String(ESP.getFreeHeap()) + " bytes");
+
+  #if defined(BOARD_HAS_PSRAM)
+    logInfo(String("PSRAM Free: ") + String(ESP.getFreePsram()) + " bytes");
+  #endif
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1200);
+
+  printBootHeader();
+  logInfo("Firmware startet");
+  printSystemInfo();
+
+  wifiSetup();
+}
+
+void loop() {
+  wifiLoop();
+
+  if (millis() - lastHeartbeat > 30000) {
+    lastHeartbeat = millis();
+    logInfo(String("Heartbeat, Uptime: ") + String(millis() / 1000) + " s, IP: " + wifiIpAddress());
+  }
+
+  delay(10);
+}
